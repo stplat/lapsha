317612abb056281c3b253c3d8f8c107f55315d49
stsp <st.platonov@gmail.com>
2020-07-04 14:19:17 +0300

    Initial commit

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -8,16 +8,16 @@ use Illuminate\Support\Facades\App;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class HttpsProtocol extends Middleware {
-  
+
   public function handle($request, Closure $next) {
     if (App::environment() === 'production') {
-      
+
       $uri = $request->getRequestUri();
-      
+
       if ($uri != '/' && $uri[strlen($uri) - 1] == '/') {
         return redirect(substr($uri, 0, strlen($uri) - 1), 301);
       }
-      
+
       $host = $request->header('host');
       if (substr($host, 0, 4) !== 'www.') {
         if (!$request->secure()) {
@@ -28,11 +28,11 @@ class HttpsProtocol extends Middleware {
         if (!$request->secure()) {
           $request->server->set('HTTPS', true);
         }
-        $request->headers->set('host', 'vecheria.ru');
+        $request->headers->set('host', 'lasushi.ru');
         return redirect()->to($request->path(), 301);
       }
     }
-    
+
     return $next($request);
   }
 }
